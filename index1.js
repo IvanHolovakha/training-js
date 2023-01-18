97852493a6ef688504d69768a77470775cbcf051
IvanHolovakha <meisuiwanreiser@gmail.com>
2023-01-18 19:44:57 +0200

    12

---
+++ index1.js
@@ -0,0 +1,10 @@
+const openTeamModalLink = document.querySelector(".js-team-modal");
+const modalTeam = document.querySelector("[data-team-modal]");
+const closeModalTeamBtn = document.querySelector(".team-modal__close-btn")
+
+openTeamModalLink.addEventListener("click", openTeamModal);
+closeModalTeamBtn.addEventListener("click", openTeamModal);
+
+function openTeamModal (){
+    modalTeam.classList.toggle("is-hidden");
+}
